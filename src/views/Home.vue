e51de76bcd8cffa80afb967afc911e0858d6852f
Huseyin Uysal <huseyin_uysal97@hotmail.com>
2023-05-31 13:32:20 +0300

    .

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,19 +8,28 @@
         <div class="flex justify-center gap-2 mt-2">
             <router-link :to="{name: 'byLetter', params: {letter}}" v-for="letter of letters" :key="letter">
                 {{ letter }}
-            
             </router-link>
         </div>
+
+        <pre>{{ ingredients }}</pre>
     </div>
 </template>
 
 <script setup>
-import { computed } from 'vue';
+import { computed, onMounted, ref } from 'vue';
 import store from '../store';
+import axiosClient from '../axiosClient.js'
 
 
-const meals = computed(() => store.state.meals)
+
 const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split("")
+const ingredients = ref([])
 
+
+onMounted(async () => {
+    const response = await axiosClient.get('/list.php?i=list')
+    ingredients.value = response.data
+    
+})
 
 </script>
